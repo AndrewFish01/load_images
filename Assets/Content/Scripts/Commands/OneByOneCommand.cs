5b5a81629ed5f068d91321b9d1fac78535e59d8e
AndrewRybakov <rybakov.andrew01@yandex.ru>
2022-07-19 02:37:04 +0700

    feature (Commands) - adding scripts with logic for one by one mode and when ready

---
+++ Assets/Content/Scripts/Commands/OneByOneCommand.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Content.Scripts.Enums;
+using Content.Scripts.Network;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Content.Scripts.Commands
+{
+    public class OneByOneCommand : ICommand
+    {
+        private List<Card> _cards;
+        private ImageDownloader _imageDownloader;
+        private CardFlipper _cardFlipper;
+        private CancellationTokenSource _cts;
+
+        public OneByOneCommand(List<Card> cards, ImageDownloader imageDownloader)
+        {
+            _cards = cards;
+            _imageDownloader = imageDownloader;
+            _cardFlipper = new CardFlipper(new Vector3(0, 90, 0), 100, 0.5f, Ease.InBack);
+            _cts = new CancellationTokenSource();
+        }
+
+        public async UniTask ExecuteAsync()
+        {
+            await _cardFlipper.FlipCardListAsync(_cards, CardSide.Back);
+            try
+            {
+                foreach (var card in _cards)
+                {
+                    var downloadImage = await _imageDownloader.DownloadImageAsync(APIConstants.PicsumPhoto, _cts.Token);
+                    card.SetImage(downloadImage);
+                    await _cardFlipper.FlipCardAsync(card, CardSide.Face);
+                }
+            }
+            catch (OperationCanceledException e)
+            {
+                if (_cts.Token == e.CancellationToken)
+                {
+                    Debug.LogError("Cancel load!");
+                    _cardFlipper.FlipCardListAsync(_cards, CardSide.Back).Forget();
+                }
+                else Debug.LogError(e);
+            }
+            finally
+            {
+                _cts.Dispose();
+                _cts = null;
+            }
+        }
+
+        public void Undo()
+        {
+            _cts?.Cancel();
+        }
+    }
+}
---
+++ Assets/Content/Scripts/Commands/WhenReadyCommand.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Content.Scripts.Enums;
+using Content.Scripts.Network;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Content.Scripts.Commands
+{
+    public class WhenReadyCommand : ICommand
+    {
+        private List<Card> _cards;
+        private ImageDownloader _imageDownloader;
+        private CardFlipper _cardFlipper;
+        private CancellationTokenSource _cts;
+
+        public WhenReadyCommand(List<Card> cards, ImageDownloader imageDownloader)
+        {
+            _cards = cards;
+            _imageDownloader = imageDownloader;
+            _cardFlipper = new CardFlipper(new Vector3(0, 90, 0), 100, 0.5f, Ease.InBack);          
+            _cts = new CancellationTokenSource();
+        }
+
+        public async UniTask ExecuteAsync()
+        {
+            await _cardFlipper.FlipCardListAsync(_cards, CardSide.Back);
+            try
+            {
+                await _cards.Select(DownloadSelector);
+            }
+            catch (OperationCanceledException e)
+            {
+                if (_cts.Token == e.CancellationToken)
+                {
+                    Debug.LogError("Cancel load!");
+                    _cardFlipper.FlipCardListAsync(_cards, CardSide.Back).Forget();
+                }
+                else Debug.LogError(e);
+            }
+            finally
+            {
+                _cts.Dispose();
+                _cts = null;
+            }
+        }
+        
+        private async UniTask DownloadSelector(Card card)
+        {
+            var downloadImage = _imageDownloader.DownloadImageAsync(APIConstants.PicsumPhoto, _cts.Token);
+            card.SetImage(await downloadImage);
+            await _cardFlipper.FlipCardAsync(card, CardSide.Face);
+        }
+
+        public void Undo()
+        {
+            _cts?.Cancel();
+        }
+    }
+}
